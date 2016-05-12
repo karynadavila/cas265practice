76752085ed9fcaab6aac28254189cd4123ffab99
Karina Davila <karina.davilarodriguez@pcc.edu>
2016-05-12 11:05:58 -0700

    Initial full commit
    Initial full commit of the original source code.

---
+++ scripts/scripts.js
@@ -0,0 +1,12 @@
+(function ($) {
+  
+  $('body').jvmobilemenu({
+    mainContent: $('.wrapper'),
+    theMenu: $('.mobile-menu'),
+    slideSpeed: 0.3,
+    menuWidth: 240,
+    position: 'right',
+    menuPadding: '20px 20px 60px'
+  });
+  
+})(jQuery);
